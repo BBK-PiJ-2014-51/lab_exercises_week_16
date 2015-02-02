8fac82256310fa1f4365009ae3c3f6229eb5befc
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-02-02 18:40:05 +0000

    added exercise 3b

--- src/exercise3/CatB.java
+++ src/exercise3/CatB.java
@@ -1,5 +1,25 @@
 package exercise3;
 
-public class CatB {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class CatB {
+	public static void main(String[] args) throws IOException{
+		for (String filename : args){
+			File file = new File(filename);
+			if (file.exists()){
+				BufferedReader in = new BufferedReader(new FileReader(file));
+				String nextLine = in.readLine();
+				while (nextLine != null){
+					System.out.println(nextLine);
+					nextLine = in.readLine();
+				}
+				in.close();
+			} else {
+				System.out.println(filename + " does not exist.");
+			}
+		}
+	}
 }
